20a3653eb5e39a6a53be513b318fa4af534bbd5c
Franck S <franck.spadotto@yahoo.com>
2026-09-12 15:50:24 +0200

    [Feature] Add Rector, Infection and Doctrine/container lint checks

---
+++ apps/api/rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonySetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames();
+    $rectorConfig->removeUnusedImports();
+
+    $rectorConfig->paths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+
+    $rectorConfig->sets([
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        LevelSetList::UP_TO_PHP_84,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::EARLY_RETURN,
+        SetList::PRIVATIZATION,
+    ]);
+};
